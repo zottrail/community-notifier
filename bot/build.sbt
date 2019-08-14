ee816551f516afc76883fe51155e406adf9f79ff
Chau Nguyen <chautnguyen96@gmail.com>
2019-08-14 00:28:30 -0700

    Migrate from ujson to circe

--- bot/build.sbt
+++ bot/build.sbt
@@ -4,8 +4,21 @@ version := "0.1"
 
 scalaVersion := "2.13.0"
 
+val sttpVersion = "1.6.4"
+val circeVersion = "0.12.0-RC2"
+
 libraryDependencies ++= Seq(
-  "com.lihaoyi" %% "ujson" % "0.7.5",
-  "com.softwaremill.sttp" %% "core" % "1.6.4",
   "com.github.pureconfig" %% "pureconfig" % "0.11.1"
 )
+
+libraryDependencies ++= Seq(
+  "com.softwaremill.sttp" %% "core",
+  "com.softwaremill.sttp" %% "circe"
+).map(_ % sttpVersion)
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+
--- bot/src/main/scala/Main.scala
+++ bot/src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 import java.nio.charset.StandardCharsets
 
-import pureconfig.generic.auto._
 import com.softwaremill.sttp._
+import com.softwaremill.sttp.circe._
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+import io.circe.{ Decoder, Encoder, HCursor, Json }
+import pureconfig.generic.auto._
 
 sealed trait PureConfigADT
 case class RedditOAuthCredentials(clientId: String, clientSecret: String) extends PureConfigADT
@@ -9,6 +12,19 @@ case class RedditConfig(oauth: RedditOAuthCredentials, userAgent: String) extend
 case class Config(reddit: RedditConfig)
 
 object Main {
+  implicit val backend = HttpURLConnectionBackend()
+
+  case class AccessTokenResponse(accessToken: String, expiresIn: Int)
+  implicit val decodeAccessToken: Decoder[AccessTokenResponse] = new Decoder[AccessTokenResponse] {
+    final def apply(c: HCursor): Decoder.Result[AccessTokenResponse] =
+      for {
+        accessToken <- c.downField("access_token").as[String]
+        expiresIn <- c.downField("expires_in").as[Int]
+      } yield {
+        AccessTokenResponse(accessToken, expiresIn)
+      }
+  }
+
   def asBase64(str: String): String = java.util.Base64.getEncoder.encodeToString(str.getBytes(StandardCharsets.UTF_8))
 
   def main(args: Array[String]): Unit = {
@@ -21,7 +37,7 @@ object Main {
       case Right(r) => r // TODO: Add more return variables.
     }
 
-    val req = sttp
+    val request = sttp
       .post(uri"https://www.reddit.com/api/v1/access_token?grant_type=client_credentials")
       .headers(
         Map(
@@ -34,17 +50,18 @@ object Main {
         )
       )
       .body() // Avoid 411 (Content-Length: 0) errors.
+      .response(asJson[AccessTokenResponse])
 
-    implicit val backend = HttpURLConnectionBackend()
-    val res = req.send()
+    val response = request.send()
+    val body = response.unsafeBody
 
-    if (res.code == 200) {
-      val parsed = ujson.read(res.unsafeBody).obj
-      val accessToken = parsed("access_token").str
-      val expiresIn = parsed("expires_in").num
-      println(accessToken, expiresIn)
-    } else {
-      // TODO: throw error?
+    body match {
+      case Left(l) => {
+        println("decodable error:", l)
+      }
+      case Right(r) => {
+        println(r.accessToken, r.expiresIn)
+      }
     }
   }
 }
