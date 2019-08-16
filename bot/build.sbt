e2d678002b44f7c16656b02b90bae830795b0b27
Chau Nguyen <chautnguyen96@gmail.com>
2019-08-16 14:53:18 -0700

    Downgrade to 2.12 and use IOApp + pureconfig-cats-effect

--- bot/build.sbt
+++ bot/build.sbt
@@ -2,18 +2,23 @@ name := "bot"
 
 version := "0.1"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.12.0"
 
 val sttpVersion = "1.6.4"
+val pureconfigVersion = "0.11.1"
 val circeVersion = "0.12.0-RC2"
 
+libraryDependencies += "org.typelevel" %% "cats-effect" % "2.0.0-RC1"
+
 libraryDependencies ++= Seq(
-  "com.github.pureconfig" %% "pureconfig" % "0.11.1"
-)
+  "com.github.pureconfig" %% "pureconfig",
+  "com.github.pureconfig" %% "pureconfig-cats-effect"
+).map(_ % pureconfigVersion)
 
 libraryDependencies ++= Seq(
   "com.softwaremill.sttp" %% "core",
-  "com.softwaremill.sttp" %% "circe"
+  "com.softwaremill.sttp" %% "circe",
+  "com.softwaremill.sttp" %% "async-http-client-backend-cats"
 ).map(_ % sttpVersion)
 
 libraryDependencies ++= Seq(
--- bot/src/main/scala/Main.scala
+++ bot/src/main/scala/Main.scala
@@ -1,22 +1,29 @@
 import java.nio.charset.StandardCharsets
 
+import cats.effect.{ContextShift, ExitCode, IO, IOApp}
 import com.softwaremill.sttp._
+import com.softwaremill.sttp.asynchttpclient.cats.AsyncHttpClientCatsBackend
 import com.softwaremill.sttp.circe._
+
 import io.circe.Decoder.Result
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder, HCursor, Json}
+
 import pureconfig.generic.auto._
+import pureconfig.module.catseffect._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 sealed trait PureConfigADT
 case class RedditOAuthCredentials(clientId: String, clientSecret: String) extends PureConfigADT
 case class RedditConfig(oauth: RedditOAuthCredentials, userAgent: String) extends PureConfigADT
 case class Config(reddit: RedditConfig)
 
-object Main {
-  implicit val backend = HttpURLConnectionBackend()
+object Main extends IOApp {
+  implicit val backend: SttpBackend[IO, Nothing] = AsyncHttpClientCatsBackend[IO]()
 
   case class AccessTokenResponse(accessToken: String, expiresIn: Int)
   implicit val decodeAccessToken: Decoder[AccessTokenResponse] = new Decoder[AccessTokenResponse] {
@@ -43,43 +50,47 @@ object Main {
 
   def asBase64(str: String): String = java.util.Base64.getEncoder.encodeToString(str.getBytes(StandardCharsets.UTF_8))
 
-  def main(args: Array[String]): Unit = {
-    val response = for {
-      config <- pureconfig.loadConfig[Config]
+  val initializeConfig : IO[Config] = loadConfigF[IO, Config]
 
-      request = sttp
-        .post(uri"https://www.reddit.com/api/v1/access_token?grant_type=client_credentials")
-        .headers(
-          Map(
-            "User-agent"     -> config.reddit.userAgent,
-            "Authorization"  -> "Basic ".concat(
-              asBase64(
-                s"${config.reddit.oauth.clientId}:${config.reddit.oauth.clientSecret}"
-              )
+  def run(args: List[String]): IO[ExitCode] = for {
+    config <- initializeConfig
+    accessTokenRequest <- sttp
+      .post(uri"https://www.reddit.com/api/v1/access_token?grant_type=client_credentials")
+      .headers(
+        Map(
+          "User-agent" -> config.reddit.userAgent,
+          "Authorization" -> "Basic ".concat(
+            asBase64(
+              s"${config.reddit.oauth.clientId}:${config.reddit.oauth.clientSecret}"
             )
           )
         )
-        .body() // Avoid 411 (Content-Length: 0) errors.
-        .response(asJson[AccessTokenResponse])
-
-      response = request.send()
-      body <- response.body
-      accessTokenData <- body
-
-      request = sttp
-        .get(uri"https://oauth.reddit.com/r/uci/hot?limit=25")
-        .headers(
-          Map(
-            "User-agent" -> config.reddit.userAgent,
-            "Authorization" -> "Bearer ".concat(accessTokenData.accessToken)
-          )
+      )
+      .body() // Avoid 411 (Content-Length: 0) errors.
+      .response(asJson[AccessTokenResponse])
+      .send()
+    accessTokenResponse <- IO {
+      for {
+        accessTokenBody <- accessTokenRequest.body
+        accessTokenResponse <- accessTokenBody.right
+      } yield accessTokenResponse
+    }
+    redditListingRequest <- sttp
+      .get(uri"https://oauth.reddit.com/r/uci/hot?limit=25")
+      .headers(
+        Map(
+          "User-agent" -> config.reddit.userAgent,
+          "Authorization" -> "Bearer ".concat(accessTokenResponse.right.get.accessToken)
         )
-        .response(asJson[RedditListingResponse])
-
-      response = request.send()
-      body <- response.body
-      listingData <- body
-    } yield listingData
-    println("Response", response) // Left(error) or Right(response)
-  }
+      )
+      .response(asJson[RedditListingResponse])
+      .send()
+    redditListingResponse <- IO {
+      for {
+        redditListingBody <- redditListingRequest.body
+        redditListingResponse <- redditListingBody.right
+      } yield redditListingResponse
+    }
+    _ = println(redditListingResponse.right.get)
+  } yield ExitCode.Success
 }
